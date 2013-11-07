6a98fe549556b2acd82ddbc49df689e4f19b1079
sarak_000 <sarak_000@HTPC>
2013-11-07 12:59:20 +0100

    correction syntax errors of pull request

--- sickbeard/clients/transmission.py
+++ sickbeard/clients/transmission.py
@@ -30,9 +30,9 @@ class TransmissionAPI(GenericClient):
         super(TransmissionAPI, self).__init__('Transmission', host, username, password)
         
         if self.host.endswith(':\d{2,4}/?'):
-	    self.url = self.host + 'transmission/rpc'
-	else
-	    self.url = self.host + 'rpc' 
+            self.url = self.host + 'transmission/rpc'
+        else:
+            self.url = self.host + 'rpc' 
 
     def _get_auth(self):
 
