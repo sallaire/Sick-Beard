af18cb35ba61caebbb6882b54e81d66a3e2462be
orion1024 <orion1024@gmail.com>
2014-01-21 23:20:40 +0100

    Corrected search strings, VOSTFR eps should no longer be returned when looking for french audio eps

--- sickbeard/providers/tpi.py
+++ sickbeard/providers/tpi.py
@@ -60,7 +60,6 @@ class TPIProvider(generic.TorrentProvider):
             results.append( urllib.urlencode( {'search': searchString, 'parent_cat' : "Séries VOSTFR", 'cat': 0, 'incldead' : 0, 'lang' : 1  } ))
             results.append( urllib.urlencode( {'search': searchString, 'parent_cat' : "Séries VOSTFR", 'cat': 0, 'incldead' : 0, 'lang' : 2  } ))
         elif audio_lang == "fr" or french:
-            results.append( urllib.urlencode( {'search': searchString, 'parent_cat' : "Séries VOSTFR", 'cat': 0, 'incldead' : 0, 'lang' : 0  } ))
             results.append( urllib.urlencode( {'search': searchString, 'parent_cat' : "Séries VF", 'cat': 0, 'incldead' : 0, 'lang' : 0  } ))
             if not fullSeason: # there is a bug in TPI search, when selecting category Series Multi the search returns single episodes torrents even when we are looking for full season.
                results.append( urllib.urlencode( {'search': searchString, 'parent_cat' : "Séries Multi", 'cat': 0, 'incldead' : 0, 'lang' : 0  } ))
