28dce5d968641d171819b1ef197bbb1641ea8fcf
sarakha63 <sarakha_ludovic@yahoo.fr>
2014-03-19 21:51:52 +0100

    Update all_tests.py

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -7,7 +7,7 @@
 #
 # Sick Beard is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Fountion, either version 3 of the License, or
 # (at your option) any later version.
 #
 # Sick Beard is distributed in the hope that it will be useful,
